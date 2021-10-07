5535820c922b49369d44d9a2bd44d5619669922d
bthall73 <bthall@live.unc.edu>
2021-10-07 19:23:09 -0400

    Working now?

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -49,7 +49,7 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-   delete object.key;
+   delete object.key(key);
    return object;
 }
 
@@ -70,7 +70,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-
+   
 }
 
 /**
